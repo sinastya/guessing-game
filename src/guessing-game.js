8c14189106ec578978e1cd6c04bf18beb4232882
sinastya <sinastya@yandex.ru>
2022-07-23 00:59:24 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.res = Math.round((this.max - this.min)/ 2 + this.min);
+      return this.res
     }
 
     lower() {
-
+      this.setRange(this.min, this.res)
     }
 
     greater() {
-
+      this.setRange(this.res, this.max)
     }
 }
 
